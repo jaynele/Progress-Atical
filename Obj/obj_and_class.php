f28ed781d606dcfb37cd026bafebed4544ff6765
jaynele <test@runoob.com>
2017-07-29 12:10:09 +0800

    对象和类的关系

---
+++ Obj/obj_and_class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/7/29
+ * Time: 12:00
+ */
+
+namespace space1;
+
+
+class Associa
+{
+    public function a(){
+        echo '类和对象';
+        echo '类是生成对象的代码模板';
+        echo 'class 类名大驼峰 花括号';
+        echo '对象是类的实例，new 类型大驼峰 小括号';
+        echo '同一个类生成的相同类型的不同对象';
+    }
+}
