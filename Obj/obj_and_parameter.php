b2918e844513702e6ae535efb4d816cabf0a019b
jaynele <test@runoob.com>
2017-07-29 13:14:40 +0800

    对象方法中传参对象的参数类型

---
+++ Obj/obj_and_parameter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/7/29
+ * Time: 13:12
+ */
+
+namespace space2;
+
+
+class obj_and_parameter
+{
+    public function a(){
+        echo '对象方法中当传参为对象时候，限定对象这个参数类型';
+        echo '方法圆括号中传参对象的类名 对象';
+    }
+}
