98eea1e098d3533fea07b9997868b213c4766229
jaynele <test@runoob.com>
2017-07-29 10:45:40 +0800

    'PHP基础之---git简单命令'

--- PHP/php_git_useage.php
+++ PHP/php_git_useage.php
@@ -5,3 +5,15 @@
  * Date: 2017/7/29
  * Time: 10:37
  */
+
+git init
+git pull
+git status
+git add
+git status
+git commit -m ''
+git remote add origin
+git push origin master
+git config --global user.name jaynele
+git config --global user.email jaynele@qq.com
+git reset HEAD
