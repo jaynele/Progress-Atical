72e2f8f3119aecb0d24b4a44f8e38cc518240216
jaynele <test@runoob.com>
2017-07-30 20:17:28 +0800

    高级特性之----抽象类

--- Obj/obj_and_abstract.php
+++ Obj/obj_and_abstract.php
@@ -11,7 +11,8 @@ namespace space2;
 
 class obj_and_abstract
 {
-    public function a(){
+    public function a()
+    {
         echo '抽象类abstract';
         echo '抽象类至少有一个抽象方法';
         echo '抽象类子类必须完整每个抽象方法，抽象子类可以不用';
