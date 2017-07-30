8169ac2a51256dcc39d6698f814a4febbed235d3
jaynele <test@runoob.com>
2017-07-30 20:44:12 +0800

    高级特性之----接口

---
+++ Obj/obj_and_interface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/7/30
+ * Time: 20:40
+ */
+
+namespace space2;
+
+
+class obj_and_interface
+{
+    public function a()
+    {
+        echo '接口 interface implements';
+        echo '一个类可以同时继承一个父类多个接口，父类extends 需要在 多个接口implements前面，如下：';
+    }
+}
