f41196ae8922952082e893331593f236d8f9960f
jaynele <test@runoob.com>
2017-08-02 17:59:21 +0800

    高级特性之----回调，匿名函数，闭包

---
+++ Obj/obj_and_callback.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/8/2
+ * Time: 17:57
+ */
+
+namespace space2;
+
+
+class obj_and_callback
+{
+    public function a(){
+        echo '回调，匿名函数，闭包';
+    }
+}
