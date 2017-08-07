eb264bdbb5f831240748df9bbd018466786e42ca
jaynele <test@runoob.com>
2017-08-07 16:52:27 +0800

    'api'

---
+++ ObjTool/obj_and_api2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/8/4
+ * Time: 16:26
+ */
+
+namespace obj_tool;
+
+
+class obj_and_api2
+{
+    public function a(){
+        echo '';
+    }
+}
