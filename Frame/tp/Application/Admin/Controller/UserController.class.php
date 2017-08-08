d2e123e3b792ade71ac18ad00279dc9f77e8184d
jaynele <test@runoob.com>
2017-08-08 10:57:07 +0800

    'server'

---
+++ Frame/tp/Application/Admin/Controller/UserController.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/8/8
+ * Time: 9:41
+ */
---
+++ SERVER/server_func.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/8/8
+ * Time: 10:42
+ */
+class func{
+    static  public function ser(){
+        var_dump($_SERVER['HTTP_HOST']);//本地客户端localhost
+        var_dump($_SERVER['REQUEST_URI']);//www/git/index.php
+        var_dump($_SERVER['REMOTE_ADDR']);//真实客户端::1
+//        var_dump($_SERVER['REMOTE_HOST']);
+        var_dump($_SERVER['REMOTE_PORT']);//端口
+        var_dump($_SERVER['REQUEST_METHOD']);//请求方法get
+        var_dump($_SERVER['REQUEST_TIME']);//请求时间
+        var_dump($_SERVER['DOCUMENT_ROOT']);//根路径
+        var_dump($_SERVER['HTTP_REFERER']);//整个请求路径目录
+    }
+}
+func::ser();
