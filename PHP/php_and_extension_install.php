8a9c5d0c7a7f4e9b4b3114ba04ef4a7d02acaf4e
jaynele <test@runoob.com>
2017-08-14 15:22:42 +0800

    'update'

---
+++ PHP/php_and_extension_install.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/8/14
+ * Time: 14:46
+ */
+echo www.openssl.org/source/;
+echo 获取最新版openssl，卸载旧版本openssl;
+echo 进入解压目录
+echo ./config --prefix=/usr/local/openssl
