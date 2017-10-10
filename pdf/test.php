b740a20adef66a3a58323469c2e2b4215f851823
jaynele <test@runoob.com>
2017-10-10 21:36:21 +0800

    'update'

--- pdf/test.php
+++ pdf/test.php
@@ -13,4 +13,26 @@
     // 逐页提取文本
     foreach($pages as $page){
         echo($page->getText());
-    }   
+    }
+
+
+
+
+
+// Include Composer autoloader if not already done.
+include 'vendor/autoload.php';
+
+// Parse pdf file and build necessary objects.
+$parser = new \Smalot\PdfParser\Parser();
+$pdf    = $parser->parseFile('document.pdf');
+
+// Retrieve all details from the pdf file.
+$details  = $pdf->getDetails();
+
+// Loop over each property to extract values (string or array).
+foreach ($details as $property => $value) {
+    if (is_array($value)) {
+        $value = implode(', ', $value);
+    }
+    echo $property . ' => ' . $value . "\n";
+}
