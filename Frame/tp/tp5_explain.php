60b10280a1bfa996e03cc075b80d9d5bda34917a
jaynele <test@runoob.com>
2017-10-11 12:01:52 +0800

    'update'

---
+++ Frame/tp/tp5_explain.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 123
+ * Date: 2017/10/11
+ * Time: 11:48
+ */
+echo '
+
+（一）路由定义
+
+（二）控制器
+（1）
+继承think下面的控制器
+use think\Controller;
+
+class Index extends Controller
+
+（2）
+控制器里面初始时候加载
+_initialize
+
+（3）
+控制器beforeActionList属性可以指定某个方法为其他方法的前置操作
+数组键名为需要调用的前置方法名，所以需要调用的前置方法都为受保护的，无值的话为当前控制器下所有方法的前置方法。
+
+
+
+
+';
