491eeda1e31aa3a42cd49bc06fa804f1c625fbf6
= <=>
2017-11-10 10:12:36 +0800

    'update'

--- PHP/Str/1Str.php
+++ PHP/Str/1Str.php
@@ -5,19 +5,26 @@
  * Date: 2017/10/19
  * Time: 21:10
  */
-echo '
 
-1、字符串翻转
-    strrev
-
-2、字符串长度,包含考虑中文，所以用mb
-    mb_strlen
-
-3、中文字符截取无乱码
-    mb_substr
-
-4、
+//字符串翻转
+strrev();
 
 
+//考虑中文获取字符串长度
+mb_strlen();
 
-';
+
+//考虑中文字符串截取
+mb_substr();
+
+
+//字符串全转大写
+strtoupper();
+
+
+//字符串全转小写
+strtolower();
+
+
+//参数&拼接
+http_build_query();
