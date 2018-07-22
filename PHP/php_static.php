f633b3a73e99233be1a33e79d76be897d8e451a2
ljl <jayneleliu@gmail.com>
2018-07-22 21:51:00 +0800

    调整

--- PHP/php_static.php
+++ PHP/php_static.php
@@ -1,8 +1,7 @@
 <?php 
-echo '<p>Static 作用域</p>';
- ?>
+echo '1/<p>Static 作用域 当一个函数完成时，它的所有变量通常都会被删除。然而，有时候您希望某个局部变量不要被删除。
+要做到这一点，请在您第一次声明变量时使用 static 关键字</p>';
 
- <?php
 function myTest()
 {
     static $x=0;
@@ -13,10 +12,35 @@ function myTest()
 myTest();
 myTest();
 myTest();
-?>
 
-<?php 
 
-echo '当一个函数完成时，它的所有变量通常都会被删除。然而，有时候您希望某个局部变量不要被删除。
-要做到这一点，请在您第一次声明变量时使用 static 关键字：';
- ?>
+echo '2/<p>global关键字</p>';
+
+$x=5;
+$y=10;
+ 
+function myTest()
+{
+    global $x,$y;
+    $y=$x+$y;
+}
+ 
+myTest();
+echo $y; // 输出 15
+
+
+echo 'global 关键字用于函数内访问全局变量。
+在函数内调用函数外定义的全局变量，我们需要在函数中的变量前加上 global 关键字：';
+echo 'PHP 将所有全局变量存储在一个名为 $GLOBALS[index] 的数组中。
+index 保存变量的名称。这个数组可以在函数内部访问，也可以直接用来更新全局变量。';
+
+$x=5;
+$y=10;
+ 
+function myTessst()
+{
+    $GLOBALS['y']=$GLOBALS['x']+$GLOBALS['y'];
+}
+
+myTessst();
+echo $y;
--- PHP/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Created by PhpStorm.
- * User: 123
- * Date: 2017/9/13
- * Time: 14:36
- */
-
